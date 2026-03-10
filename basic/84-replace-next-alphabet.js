8c0dfa63064e41104f3d18d438f6513097eff9d5
JRizzetto <jeffersonrizzetto@gmail.com>
2026-03-10 19:54:07 -0300

    JavaScript-exercices---w3resource => Exercícios 84

--- basic/84-replace-next-alphabet.js
+++ basic/84-replace-next-alphabet.js
@@ -6,22 +6,38 @@ Note: 'a' will be replace by 'b' or 'z' would be replaced by 'a'.
 */
 
 function replaceChar(str) {
-  for (let i = 0; i < str.length; i++) {
-    switch (str[i]) {
+  let textArray = str.split("");
+
+  for (let i = 0; i < textArray.length; i++) {
+    // console.log(String.fromCharCode(1 + textArray[i].charCodeAt(0)));
+    switch (textArray[i]) {
       case " ":
         break;
       case "z":
-        str[i] = "a";
+        textArray[i] = "a";
         break;
       case "Z":
-        str[i] = "A";
+        textArray[i] = "A";
         break;
       default:
-        str[i] = String.fromCharCode(str[i].charCodeAt(0) + 1);
+        textArray[i] = String.fromCharCode(1 + textArray[i].charCodeAt(0));
     }
   }
 
-  return str;
+  return textArray;
 }
 
-console.log(replaceChar("abcdz"));
+// console.log(replaceChar("abcdz"));
+
+function alphabet_char_Shift(str) {
+  let all_chars = str.split("");
+
+  for (let i = 0; i < all_chars.length; i++) {
+    let n = all_chars[i].charCodeAt() - "a".charCodeAt();
+    n = (n + 1) % 26;
+    all_chars[i] = String.fromCharCode("a".charCodeAt() + n);
+  }
+
+  return all_chars.join("");
+}
+console.log(alphabet_char_Shift("abcdxyz"));
