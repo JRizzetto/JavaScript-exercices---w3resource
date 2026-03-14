007845a9ea6be560ac5446f6a067e317e058e127
JRizzetto <jeffersonrizzetto@gmail.com>
2026-03-14 11:16:03 -0300

    JavaScript-exercices---w3resource => Exercício 97 - Tentando entender o palíndromo

--- basic/97-shortest-string-convert-palindrome.js
+++ basic/97-shortest-string-convert-palindrome.js
@@ -21,8 +21,34 @@ function palindromeString(str) {
   }
 }
 
-console.log(palindromeString("race"));
-console.log(palindromeString("ab"));
-console.log(palindromeString("abc"));
-console.log(palindromeString("abba"));
-console.log(palindromeString("abcd"));
+// console.log(palindromeString("race"));
+// console.log(palindromeString("ab"));
+// console.log(palindromeString("abc"));
+// console.log(palindromeString("abba"));
+// console.log(palindromeString("abcd"));
+
+function build_Palindrome(new_str) {
+  let flag;
+
+  for (let i = new_str.length; ; i++) {
+    flag = true;
+
+    for (let j = 0; j < i - j - 1; j++) {
+      if (i - j - 1 < new_str.length && new_str[j] != new_str[i - j - 1]) {
+        flag = false;
+        break;
+      }
+    }
+
+    if (flag) {
+      for (let j = new_str.length; j < i; j++) {
+        new_str += new_str[i - j - 1];
+      }
+      return new_str;
+    }
+  }
+}
+
+// console.log(build_Palindrome("abcddc")); // Output the result for the given string
+// console.log(build_Palindrome("122")); // Output the result for the given string
+console.log(build_Palindrome("race"));
