005cbc5fd51472357f08d0ff17effcae2e5bde04
JRizzetto <jeffersonrizzetto@gmail.com>
2026-03-18 21:16:14 -0300

    JavaScript-exercices---w3resource => Exercício 105

--- basic/105-replace-number-until-single-digit.js
+++ basic/105-replace-number-until-single-digit.js
@@ -7,7 +7,18 @@ This is until it converts to a single-digit number.
 */
 
 function singleDigit(num) {
-  console.log(num);
+  let result = 0;
+
+  while (num > 0) {
+    result += num % 10;
+    num = Math.floor(num / 10);
+  }
+
+  if (result >= 10) {
+    return singleDigit(result);
+  }
+
+  return result;
 }
 
 console.log(singleDigit(123));
